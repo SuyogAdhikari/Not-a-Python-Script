1c506f7a58082abc93381d2c2498da08d7b1dd4c
forty4minutes <aliceadhk05@gmail.com>
2020-01-07 20:16:35 +0545

    Not updated anything

--- It-does-nothing-try-it.py
+++ It-does-nothing-try-it.py
@@ -4,7 +4,7 @@ import random
 
 while True:
     nameOfWebSite = random.choice(['111111111111111111111111111111111111111111111111111111111111.com','stealthboats.com','ninjaflex.com','thatsthefinger.com','crossdivisions.com','movenowthinklater.com'])
-    agadiko = "http://{}".format(nameOfWebSite)
-    webbrowser.open(agadiko)
+    SitekoAttachments = "http://{}".format(nameOfWebSite)
+    webbrowser.open(SitekoAttachments)
     timedifference = random.randrange(5,9)
     time.sleep(timedifference)
