16a8ecc4a6382d2739f1f954c1d9e8379ba77001
forty4minutes <39236988+forty4minutes@users.noreply.github.com>
2020-01-19 13:25:05 +0545

    Update It-does-nothing-to-your-browser.py

--- It-does-nothing-to-your-browser.py
+++ It-does-nothing-to-your-browser.py
@@ -6,5 +6,5 @@ while True:
     nameOfWebSite = random.choice(['111111111111111111111111111111111111111111111111111111111111.com','stealthboats.com','ninjaflex.com','thatsthefinger.com','crossdivisions.com','movenowthinklater.com'])
     SitekoAttachments = "http://{}".format(nameOfWebSite)
     webbrowser.open(SitekoAttachments)
-    timedifference = random.randrange(5,9)
+    timedifference = random.randrange(0.5,2)
     time.sleep(timedifference)
